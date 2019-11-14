e8b923f41a05f80b86e1352c83cb19f6be6e8b26
madprops <easywest@protonmail.com>
2019-11-14 13:48:17 -0600

    Comment

--- src/lester.nim
+++ src/lester.nim
@@ -8,6 +8,7 @@ import strformat
 # Get the config object
 var conf = get_config()
 
+# Show docs path
 echo &"\n{ansiForegroundColorCode(fgBlue)}Docs Path: {ansiResetCode}",
     &"{$conf.docs_path.replace(gethomedir(), \"~/\")}"
 
@@ -17,6 +18,7 @@ if conf.paths.len() == 0:
     conf.paths = ask_paths(conf)
     echo ""
 
+# Process each path
 for path in conf.paths:
     process_path(conf, path)
 
